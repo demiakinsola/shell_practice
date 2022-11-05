4724c513c0e1f1e12fafaabd8546fd9d4c37feeb
demiakinsola <oluwademiladeakinsola@gmail.com>
2022-11-05 12:57:46 -0700

    Created a progam that prints "$ ", wait for the user to enter a command, prints it on the next line.

---
+++ read_line.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - This program prints all the arguments, without using ac.
+ *Return: 0 on success.
+ */
+
+int main(void)
+{
+	char **buffer;
+	size_t bytes = 64;
+	int index = 0;
+	ssize_t ret_bytes; /* returned bytes for getline func. */
+
+	buffer = malloc(sizeof(bytes)); /* allocate memory */
+	
+	while (1) /* infinite loop */
+	{
+		printf("$ ");
+		ret_bytes = getline(buffer, &bytes, stdin);
+		if (ret_bytes == -1) /* on failure */
+			break;
+		while (buffer[index])
+		{
+			printf("%s", buffer[index]);
+			index++;
+		}
+	}
+	return (0);
+}
